fc2b0b6f79ab6c08555321d707fdccc6a73c1194
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-12-17 11:14:35 +0800

    remove getArray and getObject

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import xiaoJson.type.XiaoArray
 import xiaoJson.type.XiaoObject
 
 fun main() {
-    val data = XiaoJSON.parser("{\"a\": {\"a\":\"c\", \"c\": [1,2,3.1]}}")
+    val data = XiaoJSON.parser("{\"a\": {\"a\":\"c\", \"c\": [1,2,3.1]}, \"c\": \"b\", \"d\": [{\"a\": 10}]}")
     println(data)
-    println(data.get<XiaoObject>("a"))
+    println(data.get<XiaoArray>("d")[0].get<Long>("a"))
 }
--- src/main/kotlin/util/nodes/Node.kt
+++ src/main/kotlin/util/nodes/Node.kt
@@ -8,16 +8,4 @@ abstract class Node {
             return this.value[key]?.value as T
         } else throw throw TypeCastException()
     }
-
-    fun getObject(key: String): MutableMap<String, Node> {
-        if (this is ObjectNode) {
-            return (this.value[key] as ObjectNode).value
-        } else throw throw TypeCastException()
-    }
-
-    fun getArray(key: String): MutableList<Node> {
-        if (this is ObjectNode) {
-            return (this.value[key] as ArrayNode).value
-        } else throw throw TypeCastException()
-    }
 }
